aadf84b4e33d3307e9ec65d73ec3b33aac584c39
ammar <muhammad.ammar@appiskey.com>
2024-09-05 22:15:39 +0500

    resolve

--- CryptoApp/Core/Home/ViewModel/HomeViewModel.swift
+++ CryptoApp/Core/Home/ViewModel/HomeViewModel.swift
@@ -22,7 +22,6 @@ class HomeViewModel : ObservableObject {
     private var cancellable = Set<AnyCancellable>()
     
     init() {
-<<<<<<< HEAD
        addSubscribers()
     }
     
@@ -54,14 +53,6 @@ class HomeViewModel : ObservableObject {
                 self?.isLoading = false
             }
             .store(in: &cancellable)
-        
-        
-=======
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-            self.allCoins.append(DeveloperPreview.instance.coin)
-            self.portfolioCoins.append(DeveloperPreview.instance.coin)
-        }        
->>>>>>> 2a783c7 (space)
     }
     
     private func mapAllCoins(allCoins: [CoinModel], portfolioEntities: [PortfolioEntity]) -> [CoinModel] {
